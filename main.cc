db562911924c43a98c2330f58f21fb7fda402916
Lee Salzman <lsalzman@gmail.com>
2019-12-08 17:53:10 -0500

    stubs for shader ABI

--- main.cc
+++ main.cc
@@ -1,9 +1,6 @@
 
 #include "glsl.h"
 using namespace glsl;
-vec4 gl_FragCoord;
-vec4 gl_FragColor;
-vec4 gl_Position;
 typedef uint32_t GLuint;
 typedef int32_t GLboolean;
 typedef float GLfloat;
@@ -37,8 +34,54 @@ struct VertexAttrib {
         char *buf; // XXX: this can easily dangle
 };
 
+struct ShaderImpl {
+    typedef int (ShaderImpl::*GetUniformFunc)(const char *name);
+    typedef void (ShaderImpl::*SetUniform1iFunc)(int index, int value);
+    typedef void (ShaderImpl::*SetUniform4fvFunc)(int index, const float *value);
+    typedef void (ShaderImpl::*SetUniformMatrix4fvFunc)(int index, const float *value);
+
+    GetUniformFunc get_uniform_func = nullptr;
+    SetUniform1iFunc set_uniform_1i_func = nullptr;
+    SetUniform4fvFunc set_uniform_4fv_func = nullptr;
+    SetUniformMatrix4fvFunc set_uniform_matrix4fv_func = nullptr;
+};
+
+struct VertexShaderImpl : ShaderImpl {
+    typedef void (VertexShaderImpl::*BindAttribFunc)(const char *name, int index);
+    typedef void (VertexShaderImpl::*InitBatchFunc)();
+    typedef void (VertexShaderImpl::*LoadAttribsFunc)(VertexAttrib *attribs, unsigned short *indices, int start, int instance, int count);
+    typedef void (VertexShaderImpl::*RunFunc)(char* flats, char* interps, size_t interp_stride);
+
+    BindAttribFunc bind_attrib_func = nullptr;
+    InitBatchFunc init_batch_func = nullptr;
+    LoadAttribsFunc load_attribs_func = nullptr;
+    RunFunc run_func = nullptr;
+
+    vec4 gl_Position;
+};
+
+struct FragmentShaderImpl : ShaderImpl {
+    typedef void (FragmentShaderImpl::*InitBatchFunc)();
+    typedef void (FragmentShaderImpl::*InitPrimitiveFunc)(const void* flats);
+    typedef void (FragmentShaderImpl::*InitSpanFunc)(const void* interps, const void* step);
+    typedef void (FragmentShaderImpl::*RunFunc)(const void* step);
+    typedef void (FragmentShaderImpl::*SkipFunc)(const void* step);
+    typedef bool (FragmentShaderImpl::*UseDiscardFunc)();
+
+    InitBatchFunc init_batch_func = nullptr;
+    InitPrimitiveFunc init_primitive_func = nullptr;
+    InitSpanFunc init_span_func = nullptr;
+    RunFunc run_func = nullptr;
+    SkipFunc skip_func = nullptr;
+    UseDiscardFunc use_discard_func = nullptr;
+
+    vec4 gl_FragCoord;
+    Bool isPixelDiscarded;
+    vec4 gl_FragColor;
+};
+
 template<typename T>
-void load_attrib(T& attrib, VertexAttrib &va, unsigned short *indices, int start, int instance, int count, int dest) {
+void load_attrib(T& attrib, VertexAttrib &va, unsigned short *indices, int start, int instance, int count) {
 }
 
 #include "all.h"
